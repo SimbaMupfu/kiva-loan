09734a6b3f51b0ca2496fe780ec2a07e93026b87
simba_mupfu <scmupfururirwa@gmail.com>
2023-12-16 03:38:28 +0200

    Initial commit

---
+++ KivaLoan/KivaLoanApp.swift
@@ -0,0 +1,17 @@
+//
+//  KivaLoanApp.swift
+//  KivaLoan
+//
+//  Created by Simbarashe Mupfururirwa on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct KivaLoanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
